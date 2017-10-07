7a8d5109252fa78ab1de2949485bcc3dcc58a011
Marijan Petricevic <marijan.petricevic94@gmail.com>
2017-10-07 21:24:10 +0200

    Initial Commit

---
+++ KVVlive/KVVlive.h
@@ -0,0 +1,19 @@
+//
+//  KVVlive.h
+//  KVVlive
+//
+//  Created by Marijan Petricevic on 07.10.17.
+//  Copyright © 2017 Marijan Petricevic. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KVVlive.
+FOUNDATION_EXPORT double KVVliveVersionNumber;
+
+//! Project version string for KVVlive.
+FOUNDATION_EXPORT const unsigned char KVVliveVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KVVlive/PublicHeader.h>
+
+
